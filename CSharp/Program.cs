0f0ddd803d543f0de224dad57e0f6195cfe600fb
cristianbrum <cristian.brum2@yahoo.com.br>
2021-12-20 18:43:31 -0300

    implementa interfaces

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -4,13 +4,36 @@
     {
         public static void Main(string[] args)
         {
-           Pessoa person = new();
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            Pessoa person = new();
             person.Id = 1;
             person.Nome = "Cristian";
             person.Idade = 25;
             person.Cpf = "030-300-458-15";
-            Console.Write(person.Cpf);
+            Console.WriteLine(person.Cpf);
 
+            Animal animal = new();
+
+            animal.Nome = "Gatito";
+            animal.Dono = "Maria";
+
+            ReajusteFuncionario objReajusteFuncionario = new ReajusteFuncionario();
+
+            objReajusteFuncionario.nomeFuncionario = "Cristian Brum";
+            objReajusteFuncionario.salarioFuncionario = 2078.00;
+
+            Console.WriteLine("Antes do Reajuste: ");
+            Console.WriteLine("\nNome: " + objReajusteFuncionario.nomeFuncionario);
+            Console.WriteLine("\nSalário: R$ " + objReajusteFuncionario.salarioFuncionario);
+
+            objReajusteFuncionario.Reajuste();
+            Console.WriteLine("\nDepois do Reajuste: ");
+            Console.WriteLine("\nNome: " + objReajusteFuncionario.nomeFuncionario);
+            Console.WriteLine("\nSalário: R$ " + objReajusteFuncionario.salarioFuncionario);
         }
     }
 }
---
+++ CSharp/Reajuste.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    internal interface IReajuste
+    {
+        double Reajuste();
+    }
+    class ReajusteFuncionario : IReajuste
+    {
+        public string? nomeFuncionario;
+        public double salarioFuncionario;
+
+        public double Reajuste()
+        {
+            salarioFuncionario *= 1.50;
+
+            return salarioFuncionario;
+        }
+    }
+}
